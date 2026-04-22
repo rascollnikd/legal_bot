4660910cb6f526cbf8c6424e905dd8b6de25cf00
rascollnikd <163592803+rascollnikd@users.noreply.github.com>
2026-04-23 02:08:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-BOT_TOKEN=8295719402:AAGQZyp7L4SeLG-rDawVYAPKJbqBU5H2FCg
-LOG_LEVEL=INFO
-API_BASE_URL=https://api.sudrf.ru
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+API_BASE_URL = os.getenv('API_BASE_URL', 'https://api.sudrf.ru')
